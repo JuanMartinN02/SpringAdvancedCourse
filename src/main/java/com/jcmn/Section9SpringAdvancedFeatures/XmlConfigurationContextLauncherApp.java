dfd884ef985ff5f8d08e640af95e43fa47edad25
JuanMartinN02 <juannogueiram@gmail.com>
2026-08-10 10:47:57 -0400

    Section 9 Legacy XML Config example

--- src/main/java/com/jcmn/Section9SpringAdvancedFeatures/XmlConfigurationContextLauncherApp.java
+++ src/main/java/com/jcmn/Section9SpringAdvancedFeatures/XmlConfigurationContextLauncherApp.java
@@ -1,23 +1,25 @@
 package com.jcmn.Section9SpringAdvancedFeatures;
 
-import com.jcmn.Section8SpringToManageJavaObjects.YourBusinessClass;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import com.jcmn.Section7GettingStarted.GameRunner;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Arrays;
 
-@Configuration
-@ComponentScan
+
+// ALMOST NEVER USE IN THE PRESENT, NOW WE USE ANNOTATIONS LIKE THE ONES WE'VE SEEN.
+// XML CONFIGS CAN BE SEEN IN LEGACY CODE
 public class XmlConfigurationContextLauncherApp {
 
     public static void main(String[] args) {
 
-        // We are now having the Spring Framework managing objects, autowiring them and creating them
-        try (var context = new ClassPathXmlApplicationContext(XmlConfigurationContextLauncherApp.class)){
+        try (var context = new ClassPathXmlApplicationContext("contextConfiguration.xml")) {
             Arrays.stream(context.getBeanDefinitionNames())
                     .forEach(System.out::println);
+
+            System.out.println(context.getBean("name"));
+            System.out.println(context.getBean("age"));
+
+            context.getBean(GameRunner.class).runGame();
         }
 
     }
